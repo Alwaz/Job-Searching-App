38ace16f301d744c73de31e556fe5f247f12a8d2
Alwaz <alwazkazi3@gmail.com>
2021-07-03 02:33:21 -0700

    show data on front end

--- JS/script.js
+++ JS/script.js
@@ -6,15 +6,55 @@ let search = document
     console.log(text, "whats in text");
   });
 
-//fetching data from .json file
+//defining function that will fetch data from .json file
 const getJobs = () => {
-  fetch("data.json")
+  return fetch("data.json")
     .then((response) => {
       return response.json();
     })
     .then((data) => {
-      console.log(data);
+      return data;
     });
 };
 
-getJobs();
+// render jobs data on front page
+const showJobs = (jobs) => {
+  console.log("show jobs", jobs);
+  let jobsContainer = document.querySelector(".joblist__container");
+  let jobHTML = "";
+
+  jobs.forEach((job) => {
+    return (jobHTML += ` <div class="jobTile">
+    <div class="top-section">
+      <img
+        src="${job.logo}"
+        alt=""
+      />
+      <i class="fas fa-ellipsis-h"></i>
+    </div>
+    <!-- end of top section -->
+
+    <!-- role name -->
+    <div class="role-name"><span>${job.roleName}</span></div>
+    <!-- job description -->
+    <div class="job-description">
+      <span>
+        ${job.requirements.content}
+      </span>
+    </div>
+
+    <!-- buttons -->
+    <div class="buttons">
+      <div class="button apply-button">Apply Now</div>
+      <div class="button message">Message</div>
+    </div>
+  </div>
+  <!-- end of job tile-->`);
+  });
+  jobsContainer.innerHTML = jobHTML;
+  console.log(jobHTML);
+};
+
+getJobs().then((data) => {
+  showJobs(data);
+});
